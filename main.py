36b016c2ac935e0355b88e7144f2f7f0f597ce47
AlexOso97 <ralex3426@gmail.com>
2022-04-06 20:28:31 +0300

    min

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
-from nnfs.datasets import spiral_data
+from nnfs.datasets import spiral_data, vertical_data
+from numpy import ndarray
 from layer.Layer_Dense import Layer_Dense
 from activation.ReLU import Relu
 from activation.Softmax import Softmax
@@ -9,19 +10,31 @@ from accuracy.Accuracy import Accuracy
 
 def main() -> None:
 
-    X, y = spiral_data(samples=500, classes=3)
+    X, y = spiral_data(samples=500, classes=4)
 
-    layer1 = Layer_Dense(2, 8, True)
-    layer2 = Layer_Dense(8, 3, False)
+    layer1 = Layer_Dense(2, 8, False)
+    layer2 = Layer_Dense(8, 16, False)
+    layer3 = Layer_Dense(16, 32, False)
+    layer4 = Layer_Dense(32, 16, False)
+    layer5 = Layer_Dense(16, 8, False)
+    layer6 = Layer_Dense(8, 4, False)
+
     relu = Relu()
-    sm = Softmax()
+    softmax = Softmax()
     loss_func = CategoricalCrossEntropy()
     accuracy = Accuracy()
 
     relu.activate(layer1.forward(X))
-    sm.activate(layer2.forward(relu.output))
+    relu.activate(layer2.forward(relu.output))
+    relu.activate(layer3.forward(relu.output))
+    relu.activate(layer4.forward(relu.output))
+    relu.activate(layer5.forward(relu.output))
+    softmax.activate(layer6.forward(relu.output))
 
-    loss = loss_func.calculate(sm.probabilities, y)
+    loss = loss_func.calculate(softmax.probabilities, y)
+    accuracy.calculate(softmax.probabilities, y)
+
+    print(layer1.forward(X[: 1]))
     print(loss)
 
 
